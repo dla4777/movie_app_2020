b1dab9f6035b96320b435ac94a000f8c67ecb553
Yejin Lim <dla4777@naver.com>
2020-09-24 14:06:45 +0900

    Updates

---
+++ precache-manifest.190901847716715ba4b03405fdc642d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f36e5be28136a94ca16ec5718452ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ef5f363ec88a07d9493",
+    "url": "/static/css/main.e901fc29.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef5f363ec88a07d9493",
+    "url": "/static/js/main.80491195.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
